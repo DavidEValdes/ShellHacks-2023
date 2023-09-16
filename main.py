014af77c76a1cb1820865e97e729a12af77567e2
DavidEValdes <davideduardovaldes@gmail.com>
2023-09-16 03:56:34 -0400

    Changed port number

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_gmail_service():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_local_server(port=8080)
             with open('token.json', 'w') as token:
                 token.write(creds.to_json())
     return build('gmail', 'v1', credentials=creds)

4017a2ebc3fee23445655f5b185c96534cae62f9
DavidEValdes <davideduardovaldes@gmail.com>
2023-09-17 05:05:38 -0400

    Removed redundant css

--- my-app/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Carousel from './App'; // Import your Carousel component
-import './index.css';
-
-ReactDOM.render(
-  <Carousel />,
-  document.getElementById('root')
-);

1c911c165a31a28568d88f3c09114b4fed4475f6
DavidEValdes <davideduardovaldes@gmail.com>
2023-09-17 04:14:33 -0400

    Complete and FInal UI implementations

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -29,12 +29,22 @@ function App({ x, y, z }) {
   return (
     <div className="app">
       <div className="title">InternTrack</div>
+      <div className="sub-header-description">
+  Less time sifting emails, more time on what matters
+</div>
+      <div className="description">
+Automatically scan your last 10 emails and let our advanced AI determine their relevance to your internship or job search. 
+    </div>
+    <h1 className="sub-header">Email Processing Result:</h1>
+      <EmailProcessor />
       <div className="status-box">
         <div className="status-item accepted" style={{ color: 'green' }}>Relevant: {x}</div>
         <div className="status-item rejected" style={{ color: 'red' }}>Irrelevant: {y}</div>
+        <div className="info-box"></div>
+        <div className="fun-fact">
+        Fun Fact: A report from McKinsey & Company found that people spend approximately 28% of their workweek just on email.
+</div>
       </div>
-      <h1>Email Processing Result</h1>
-      <EmailProcessor />
     </div>
   );
 }
